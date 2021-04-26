1124754341b5406cc8b0b7e3af87c9c98ae1fd06
Pablo Narducci <pablonarducci@gmail.com>
2021-04-26 20:11:50 -0300

    Moved the web configuration to the umbrella application

--- config/config.exs
+++ config/config.exs
@@ -9,13 +9,22 @@
 # move said applications out of the umbrella.
 import Config
 
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-#
+# Configures the endpoint
+config :rsvp_web, RsvpWebWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "4VWCOm2mVEkBpF8tXRYACDQ3c71U1K1+N5FZ6RXrSmiGMDh6ua6VnVGkVA2lsTrf",
+  render_errors: [view: RsvpWebWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: RsvpWeb.PubSub,
+  live_view: [signing_salt: "0MwJLjy8"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
 config :rsvp, ecto_repos: [Rsvp.Repo]
 
 # Import environment specific config. This must remain at the bottom
