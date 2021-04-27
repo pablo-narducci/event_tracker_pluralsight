402f4a2791102ad11877d16bd3a72532893c7ab4
Pablo Narducci <pablonarducci@gmail.com>
2021-04-26 21:59:19 -0300

    Created the list events page

--- apps/rsvp_web/lib/rsvp_web_web/controllers/event_controller.ex
+++ apps/rsvp_web/lib/rsvp_web_web/controllers/event_controller.ex
@@ -3,8 +3,12 @@ defmodule RsvpWebWeb.EventController do
 
   def show(conn, %{"id" => id}) do
     event = Rsvp.EventQueries.get_by_id(id)
-    |> IO.inspect()
 
     render conn, "details.html", event: event
+  end
+
+  def list(conn, _params) do
+      events = Rsvp.EventQueries.get_all()
+      render conn, "list.html", events: events
   end
 end
